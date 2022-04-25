331d061354dc273e6a3e7d140fc1f064dc40522d
TomasCv0628 <tomas2224508@uis.edu.co>
2022-04-25 09:24:37 -0500

    Desarrollo: numeros-pares-impares. Diagrama y codigo. TomasCv0628

---
+++ Números-Pares-impares.py
@@ -0,0 +1,19 @@
+
+
+
+
+
+# Input
+
+X = int(input("Inserte un número: "))
+
+# Processing
+
+if X % 2 == 0: 
+    msj = ("Es un numero par")
+else:
+    msj = ("Es un numero impar")
+# Output
+
+
+print("EL numero " + str(X) + "es: " + msj) 
---
+++ README.md
@@ -0,0 +1,10 @@
+# DETECTAR NÚMEROS PARES - IMPARES
+
+## Definición de variables
+
+X = cualquier numero enteros
+
+### Diagrama de flujo
+
+![Diagrama de flujo](numeros-pares-impares.png "Diagrama de flujo")
+
